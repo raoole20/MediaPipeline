c825122f83b6052b195dabcd5d7b794a6ccc75d2
raul espina <respina@bigwise.com>
2025-12-01 16:34:51 -0400

    feat: Scaffold file management module with CRUD functionality, DTOs, and API setup.

--- rest-api/src/app.module.ts
+++ rest-api/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FilesModule } from './files/files.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    FilesModule,
+    ConfigModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
 
-  // TODO: configurar global pipes
 
   const config = new DocumentBuilder()
     .setTitle('REST API for files management')
@@ -16,9 +16,19 @@ async function bootstrap() {
     .addTag('files')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api/docs', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  await app.listen(process.env.PORT ?? 3000)
+    .then(() => console.log(`Server running on port ${process.env.PORT}`))
+    .catch((error) => console.error(error));
 }
 
 bootstrap();
