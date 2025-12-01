38ede8a81b78b5708953b64f6e9ac2d0df581c37
raul espina <respina@bigwise.com>
2025-12-01 16:23:02 -0400

    feat: Initialize NestJS REST API with basic configuration and Swagger documentation.

--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  // TODO: configurar global pipes
+
+  const config = new DocumentBuilder()
+    .setTitle('REST API for files management')
+    .setDescription('REST API for files management')
+    .setVersion('0.0.1')
+    .addTag('files')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
