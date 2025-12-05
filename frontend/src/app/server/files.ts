16018dc7b515ec11c8178a43dad3f9e7b9fcbe43
raul espina <espinaraul8@gmail.com>
2025-12-04 20:58:35 -0400

    feat: implement file upload logic in uploadFile function; add fetch request to handle file uploads

--- frontend/src/app/server/files.ts
+++ frontend/src/app/server/files.ts
@@ -7,5 +7,9 @@
 export async function uploadFile(file: File) {
     // Aquí puedes implementar la lógica para manejar el archivo en el servidor
     // Por ejemplo, guardarlo en una base de datos o en un sistema de archivos
+    fetch(`${process.env.API_URL || ''}/api/upload`, {
+        method: 'POST',
+        body: file,
+    });
     console.log(`Archivo recibido: ${file.name}, tamaño: ${file.size} bytes`);
 }
